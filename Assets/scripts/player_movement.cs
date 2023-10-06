296eaf46a783c476de4df9cbc6dd4efa0046df61
mitchell <dont@contact.me>
2023-10-05 22:35:36 -0400

    added x-axis movement

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -2,6 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_movement : MonoBehaviour {
 
+public class player_movement : MonoBehaviour {
+	public float acceleration;
+	public float friction;
+
+	private float latest_move_x;
+
+	private fixed_position_helper positioner;
+
+	void Start() {
+		latest_move_x = 0.00f;
+
+		positioner.initialize();
+		positioner.fixed_timestep = Time.fixedDeltaTime;
+		positioner.acceleration = acceleration;
+		positioner.friction = new Vector3(1, 0, 1) * friction;
+		positioner.gravity  = new Vector3(0.00f, 0.00f, 0.00f);
+	}
+
+	void Update() {
+		latest_move_x = move_x();
+		transform.position += positioner.interpolated_move(Time.time);
+	}
+
+	void FixedUpdate() {
+		positioner.accelerate(new Vector3(latest_move_x, 0, 0));
+		transform.position += positioner.tick();
+	}
+
+	private float move_x() { return Input.GetAxisRaw("horizontal"); }
 }
